457a77187b144b8adaff5a929f8dfb094af98900
Erin <erincoffey@mac.com>
2018-01-12 11:48:07 -0800

    improve indentation and add global contant to future value program

--- exersizes/Future_Value.py
+++ exersizes/Future_Value.py
@@ -4,28 +4,30 @@
 # by Erin Coffey
 # 11 January 2018
 
+AUTHOR = "Erin"
+
 def calculate_future_value(monthly_investment, yearly_interest_rate, years=20):
 
- # convert yearly values to monthly values
- monthly_interest_rate = yearly_interest_rate / 12 / 100
- months = years * 12
+  # convert yearly values to monthly values
+  monthly_interest_rate = yearly_interest_rate / 12 / 100
+  months = years * 12
 
- # calculate future value
- total_investment = future_value = 0
+  # calculate future value
+  total_investment = future_value = 0
  
- for i in range(months):
+  for i in range(months):
      future_value += monthly_investment
      monthly_interest_amount = future_value * monthly_interest_rate
      future_value += monthly_interest_amount
      total_investment += monthly_investment
 
- return future_value, total_investment
+  return future_value, total_investment
 # end calculate_future_value
 
 def show_welcome():
- print("*************** PYTHON **************")
- print("**   Erin\'s Future Value program   **")
- print("*************** PYTHON **************")
+  print("*************** PYTHON **************")
+  print("**   "+AUTHOR+"\'s Future Value program   **")
+  print("*************** PYTHON **************")
 # end show_welcome()
 
 def display_investment_results(years, totalInv, resulting_value):
@@ -36,47 +38,47 @@ def display_investment_results(years, totalInv, resulting_value):
 # end display_investment_results
 
 def main():
- show_welcome()
- should_Exit = False
- yearly_interest_rate = monthly_investment = 0.0
- years = 0
- while not should_Exit:
-  print()
-  # get input from the user
-  try:
-    monthly_investment = float(input("Enter monthly investment:\t"))
-  except ValueError:
-    print ("ERROR, monthly rate should be a number. Please try again.")
-    continue
-  try:
-     yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
-  except ValueError:
-    print ("ERROR, yearly interest rate should be a number. Please try again.")
-    continue
-  try:
-    years = int(input("Enter number of years:\t\t"))
-  except ValueError:
-    print ("ERROR, number of years should be an integer. Please try again.")
-    continue
+  show_welcome()
+  should_Exit = False
+  yearly_interest_rate = monthly_investment = 0.0
+  years = 0
+  while not should_Exit:
+    print()
+    # get input from the user
+    try:
+      monthly_investment = float(input("Enter monthly investment:\t"))
+    except ValueError:
+      print ("ERROR, monthly rate should be a number. Please try again.")
+      continue
+    try:
+      yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+    except ValueError:
+      print ("ERROR, yearly interest rate should be a number. Please try again.")
+      continue
+    try:
+      years = int(input("Enter number of years:\t\t"))
+    except ValueError:
+      print ("ERROR, number of years should be an integer. Please try again.")
+      continue
 
-  future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate, years)
-  display_investment_results(years, total_investment, future_value)
+    future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate, years)
+    display_investment_results(years, total_investment, future_value)
 
-  choice = input("See results for 20 year investment? (y/n): ")
-  if choice.lower() == "y":
-      # call using default value for years
-      future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate)
-      # call function using named args in alternative order
-      display_investment_results(totalInv=total_investment, resulting_value=future_value, years=20)
+    choice = input("See results for 20 year investment? (y/n): ")
+    if choice.lower() == "y":
+       # call using default value for years
+       future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate)
+       # call function using named args in alternative order
+       display_investment_results(totalInv=total_investment, resulting_value=future_value, years=20)
 
-  choice = input("Try Future Value program again? (y/n): ")
-  if choice.lower() != "y":
-     should_Exit = True
+    choice = input("Try Future Value program again? (y/n): ")
+    if choice.lower() != "y":
+      should_Exit = True
      
- # end while loop
+  # end while loop
 # end main()
- print("Bye!")
+print("Bye!")
 
 #if the current module is the main module
 if __name__ == "__main__":
- main()
+  main()
