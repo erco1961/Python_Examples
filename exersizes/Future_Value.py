d8c3b92b04f6bdf1abb49a62e7f5201bfaa0f940
Erin <erincoffey@mac.com>
2018-01-12 11:11:28 -0800

    adding function calls to future value program

--- exersizes/Future_Value.py
+++ exersizes/Future_Value.py
@@ -4,57 +4,79 @@
 # by Erin Coffey
 # 11 January 2018
 
-import sys
+def calculate_future_value(monthly_investment, yearly_interest_rate, years=20):
 
-# display a welcome message
-print("*************** PYTHON **************")
-print("**  Erin\'s Future Value program  **")
-print("*************** PYTHON **************")
-
-should_Exit = False
-yearly_interest_rate = monthly_investment = 0.0
-years = 0
-while not should_Exit:
- print()
-# get input from the user
- try:
-    monthly_investment = float(input("Enter monthly investment:\t"))
- except ValueError:
-    print ("ERROR, monthly rate should be a number. Please try again.")
-    continue
- try:
-     yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
- except ValueError:
-    print ("ERROR, yearly interest rate should be a number. Please try again.")
-    continue
- try:
-    years = int(input("Enter number of years:\t\t"))
- except ValueError:
-    print ("ERROR, number of years should be an integer. Please try again.")
-    continue
-
-# convert yearly values to monthly values
+ # convert yearly values to monthly values
  monthly_interest_rate = yearly_interest_rate / 12 / 100
  months = years * 12
 
-# calculate future value
- total_investment = 0
- future_value = 0
+ # calculate future value
+ total_investment = future_value = 0
+ 
  for i in range(months):
      future_value += monthly_investment
      monthly_interest_amount = future_value * monthly_interest_rate
      future_value += monthly_interest_amount
      total_investment += monthly_investment
 
-# display the amount
- print("Your total investment would be:\t" + str(total_investment))
- print("Future value:\t\t\t" + str(round(future_value,2)))
- print()
+ return future_value, total_investment
+# end calculate_future_value
 
- choice = input("Try Future Value program again? (y/n): ")
- if choice.lower() != "y":
+def show_welcome():
+ print("*************** PYTHON **************")
+ print("**   Erin\'s Future Value program   **")
+ print("*************** PYTHON **************")
+# end show_welcome()
+
+def display_investment_results(years, totalInv, resulting_value):
+  print("After " + str(years) + " years:\t\t")
+  print("Total investment:\t\t" + str(totalInv))
+  print("Future value:\t\t\t" + str(round(resulting_value,2)))
+  print()
+# end display_investment_results
+
+def main():
+ show_welcome()
+ should_Exit = False
+ yearly_interest_rate = monthly_investment = 0.0
+ years = 0
+ while not should_Exit:
+  print()
+  # get input from the user
+  try:
+    monthly_investment = float(input("Enter monthly investment:\t"))
+  except ValueError:
+    print ("ERROR, monthly rate should be a number. Please try again.")
+    continue
+  try:
+     yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+  except ValueError:
+    print ("ERROR, yearly interest rate should be a number. Please try again.")
+    continue
+  try:
+    years = int(input("Enter number of years:\t\t"))
+  except ValueError:
+    print ("ERROR, number of years should be an integer. Please try again.")
+    continue
+
+  future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate, years)
+  display_investment_results(years, total_investment, future_value)
+
+  choice = input("See results for 20 year investment? (y/n): ")
+  if choice.lower() == "y":
+      # call using default value for years
+      future_value, total_investment = calculate_future_value(monthly_investment, yearly_interest_rate)
+      # call function using named args in alternative order
+      display_investment_results(totalInv=total_investment, resulting_value=future_value, years=20)
+
+  choice = input("Try Future Value program again? (y/n): ")
+  if choice.lower() != "y":
      should_Exit = True
-# end while loop
-print("Bye!")
+     
+ # end while loop
+# end main()
+ print("Bye!")
 
-
+#if the current module is the main module
+if __name__ == "__main__":
+ main()
